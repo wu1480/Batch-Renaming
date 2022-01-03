ae642b8d2c4d77cc61b21d467a61cb6673db108a
wu1480 <54592482+wu1480@users.noreply.github.com>
2022-01-03 00:20:22 -0500

    Different UI Look and Feel, start panel after picking folder

--- test.java
+++ test.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
-public class test extends JPanel implements ActionListener {
+public class test extends JPanel {
     JFileChooser chooser;
     JFrame frame;
     JPanel panel;
@@ -13,7 +13,7 @@ public class test extends JPanel implements ActionListener {
         frame = new JFrame();
         panel = new JPanel();
         JButton sef = new JButton("Select a File");
-        sef.addActionListener(this);
+        sef.addActionListener(new action1());
         add(sef);
         JLabel label = new JLabel();
         label.setText("bye");
@@ -31,21 +31,46 @@ public class test extends JPanel implements ActionListener {
         frame.setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent e) {
-        chooser = new JFileChooser();
-        chooser.setCurrentDirectory(new java.io.File("."));
-        chooser.setDialogTitle("Pick a File");
-        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        chooser.setAcceptAllFileFilterUsed(false);
+    public test(boolean b) {
+        JButton accept = new JButton();
+    }
 
-        if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-            System.out.println(chooser.getSelectedFile());
-        } else {
-            System.out.println("Exit");
+    private class action1 implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            chooser = new JFileChooser();
+            chooser.setCurrentDirectory(new java.io.File("."));
+            chooser.setDialogTitle("Pick a File");
+            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            chooser.setAcceptAllFileFilterUsed(false);
+
+            if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
+                System.out.println(chooser.getSelectedFile());
+                test second = new test(true);
+
+            } else {
+                System.out.println("Exit");
+            }
+        }
+    }
+
+    private class action2 implements ActionListener {
+        public void actionPerformed (ActionEvent e) {
+
         }
     }
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
         test t = new test();
     }
 }
